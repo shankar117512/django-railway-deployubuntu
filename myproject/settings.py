7bf3d4409cc33a8e648d55fe8008b451961d4373
shankar117512 <shankar117512@gmail.com>
2026-02-11 11:20:37 +0000

    Dtabase url addeed

--- myproject/settings.py
+++ myproject/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))   
+    'default': dj_database_url.config(default=os.environ.get('postgresql://postgres:fnfPkJAxflrkUHntALljDMlAoXyxGfmy@postgres.railway.internal:5432/railway'))   
     }
 }
 
